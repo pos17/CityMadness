4e4f309e4b85f56e4ee757d5ef119a8784bb6ff1
pox17 <paos9904@gmail.com>
2023-09-19 18:06:28 +0200

    updated l system to work correctly with map improvements

--- CompleteProject/PythonServer/testOSC.py
+++ CompleteProject/PythonServer/testOSC.py
@@ -2,6 +2,7 @@ from pythonosc import osc_message_builder
 from pythonosc import udp_client
 from pythonosc import osc_server
 from pythonosc import dispatcher
+import sched, time
 
 
 outport = 57120
@@ -16,4 +17,9 @@ def main():
     client = udp_client.SimpleUDPClient("127.0.0.1", outport)
     client.send_message("/noteOn",midiValue)
     print("/noteOn",midiValue)
+    scheduler = sched.scheduler(time.monotonic, time.sleep)
+    noteOnEvent = scheduler.enter(5,1,print,argument=("midiValue,client"))
+    print(scheduler.queue)
+    scheduler.cancel(scheduler.queue[0])
+    print(scheduler.queue)
 main()
