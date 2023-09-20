2a1a6b6ed496e9312f6942ceee01a2bd4f88856e
pox17 <paos9904@gmail.com>
2023-09-20 12:46:06 +0200

    binded to \reset

--- CompleteProject/PythonServer/mdp.py
+++ CompleteProject/PythonServer/mdp.py
@@ -403,7 +403,7 @@ if __name__ == "__main__":
     dispatcher.map("/currentNode", interestPathHandler)
     frase = "ciao"
     dispatcher.map("/currentNode", l_system.update_L_system, [nodes,client2,scheduler,endingTime,l_system_started,scheduler_started_time,axiom]) #function for updating l_system
-    dispatcher.map("/sendNoiseOn", l_system.sendNoiseOn, client2)
+    dispatcher.map("/reset", l_system.sendNoiseOn, client2)
     l_system.sendNoiseOn(client2)
     server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
     #print("These are the nodes")
